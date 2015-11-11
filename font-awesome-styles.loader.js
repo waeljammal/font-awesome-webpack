5f66174f7c033f78a1357bc239da2f5af5aa4ee4
Wael Jammal <wael.jammal@blackswan.com>
2015-11-11 16:11:24 +0000

    Fixed extensions

--- font-awesome-styles.loader.js
+++ font-awesome-styles.loader.js
@@ -17,13 +17,13 @@ module.exports = function(content) {
     this.cacheable(true);
     var config = this.exec(content, this.resourcePath);
     var start =
-            "@import          \"~font-awesome/scss/variables.less\";\n"
+            "@import          \"~font-awesome/scss/variables.scss\";\n"
             + "@fa-font-path: \"~font-awesome/fonts/\";\n"
-            + "@import          \"./font-awesome.config.less\";\n";
+            + "@import          \"./font-awesome.config.scss\";\n";
     source = start + styles.filter(function(style) {
         return config.styles[style];
     }).map(function(style) {
-        return "@import \"~font-awesome/scss/" + style + ".less\";";
+        return "@import \"~font-awesome/scss/" + style + ".scss\";";
     }).join("\n");
     return source;
 };
